8ef4e7a3b3718a487a6c15c0fbda85a3d50c1ebf
damianjnc <damianjnc@gmail.com>
2019-01-18 05:34:34 +0000

    cors

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,16 @@
 // Global app controller
 
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module. Variable x is ${x}. `);
+import axios from 'axios';
+async function getResults(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = '4bb5753f7b44297963bd2babd190b826';
+    const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key} & q=${query}`);
+    const  recipes = res.data.recipes;
+    console.log(recipes);
+}
+getResults('pizza');
+
+
+// 4bb5753f7b44297963bd2babd190b826
+// https://www.food2fork.com/api/search
+// https://cors-anywhere.herokuapp.com/
--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                exlude: /node_modules/,
+                exclude: /node_modules/,
                 use: {
                     loader:'babel-loader'
                 }
